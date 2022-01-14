31b52d3d84f50d96b91d5063643fc9a8ec476ff1
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-01-13 19:12:24 -0600

    applied api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
-var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+var searchBtnEl = document.querySelector("#search-button");
+
+var cityWeather = function(city) {
+    var apiKey = "2d59efa3d16a72230e485934a5a7bc3c"
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}`
+
+    fetch(apiUrl)
+    .then(function(response){
+        response.json().then(function(data){
+
+        })
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+searchBtnEl.addEventListener("click", formSubmitHandler )
