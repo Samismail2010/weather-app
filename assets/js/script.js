5e031246bafd60737745829c469eeb2cef2b8cca
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-01-17 16:31:14 -0600

    add fetch api/no error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
-var cities = [];
 
+//fetch weather API
+var cityWeather = function(city){
+    var apiKey = "c29dbdf00b06a84cdf3735b1122c2101"
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiURL).then(function(response){
+        response.json().then(function(data){
+            console.log()
+        });
+    });
+};
