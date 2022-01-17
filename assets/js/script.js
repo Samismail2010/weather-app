1ebbffb6be04a99650eb71cd9fd6a00c7be4da6e
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-01-16 19:45:50 -0600

    no error getweather func.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,30 @@
 
-var apiKey = "2d59efa3d16a72230e485934a5a7bc3c";
 var city = "";
 var now = moment();
 var currentDate = now.format("dddd[,] MMMM Do gggg");
 var currentDay = now.format("DD");
 
-function getWeather(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+$("#dateMain").text(currentDate);
+
+$(".search").on("click", function(){
+    city = $("cityMain").val();
+    getWeather(city);
+});
+
+
+
+var getWeather = function(city) {
+    var apiKey = "09e0d7e534e41ce68ba5f2577fa5f760";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=imperial&appid=" + apiKey;
     fetch(apiUrl).then(function(response){
-        $("#cityMain").text(response.name+ ", " + response.sys.country);
+        response.json.then(function(data){ 
+            $("#cityMain").text(response.main.temp);
             $("#degreeMain").text(response.main.temp);
             $("#humidityMain").text(response.main.humidity);
             $("#windMain").text(response.wind.speed);
-            $("#iconMain").attr("src","https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+            $("#iconMain").attr(".src","https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png")
+        });
+
     });
-};
+}
+
