f71da9e6af07b6be12815fe9cbedef1a80398e73
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-01-17 18:28:32 -0600

    add save localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
+var currentWeatherEl = document.querySelector("#current-weather");
+
+
+
 
 //fetch weather API
 var cityWeather = function(city){
-    var apiKey = "c29dbdf00b06a84cdf3735b1122c2101"
-    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    var apiKey = "2d59efa3d16a72230e485934a5a7bc3c"
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
     fetch(apiURL).then(function(response){
         response.json().then(function(data){
             console.log()
         });
     });
+};
+//current weather
+var currentWeather = function(weather, searchCity){
+    currentWeatherEl.textContent = "";
+}
+var weatherIconEl = document.createElement("span");
+var weatherIcon = document.createElement("img")
+//weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`);
+
+var saveSearch = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
 };
